38c5e499debf0ebbcdd8d5c772e54128fe73ad34
xelerrrrr <xeler136@gmail.com>
2025-04-16 11:55:48 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+let messages = []; // Array to store messages
+let extraMessagesCount = 0; // Counter for extra messages
+
+
+
+
+
+
+
+
+    
+
+    // <script src="https://github.com/tmijs/tmi.js/releases/download/v1.8.4/tmi.min.js"></script>
+
+ 
+const client = new tmi.Client({
+	channels: [ 'xelerrrrr' ]
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	console.log(`${tags['display-name']}: ${message}`);
+    // if ((tags['display-name']) != "xelerrrrr") {
+        sendMessage(message, tags['display-name']);
+    // }
+});
+
+
+
